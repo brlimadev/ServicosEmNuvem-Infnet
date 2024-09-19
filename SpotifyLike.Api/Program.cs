48bba0d446d11efce2220011293c92aa6216e410
brunolima <brlima@live.com>
2024-09-18 21:58:54 -0300

    Program.cs

--- SpotifyLike.Api/Program.cs
+++ SpotifyLike.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.OpenApi.Models;
 using Spotify.Application.Conta;
 using Spotify.Application.Conta.Profile;
 using Spotify.Application.Streaming;
+using Spotify.Application.Streaming.Storage;
 using SpotifyLike.Repository;
 using SpotifyLike.Repository.Repository;
 
@@ -77,13 +78,15 @@ builder.Services.AddScoped<BandaRepository>();
 //Services
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<BandaService>();
+builder.Services.AddScoped<AzureStorageAccount>();
+
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
